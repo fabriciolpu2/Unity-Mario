5def7ad6297ddd9ceb9a235159b6b9d0a0b9ee0d
fabriciolpu2 <fabriciolpu2@gmail.com>
2017-02-14 16:05:21 -0400

    MarioWorld 01

---
+++ Assets/Script/PlayerControle.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControle : MonoBehaviour {
+
+	public Animator animacao;
+	public Rigidbody2D playerRB;
+	public int forceJump;
+
+	bool slide;
+	bool jump;
+
+
+	// slide
+	public float timeSlide;
+	public float tempoTempo;
+
+
+	// verifica chao
+	public Transform groundCheck;
+	public bool grounded;
+	public LayerMask whatIsChao;
+
+	public Transform colisor;
+
+	// Use this for initialization
+	void Start () {
+		playerRB =  GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump") && grounded == true) {
+
+			//Debug.Log ("Pulo");
+			playerRB.AddForce(new Vector2(0, forceJump));
+			if (slide == true) {
+				colisor.position = new Vector3 (colisor.position.x, colisor.position.y + 0.3f, colisor.position.z);
+			}
+			slide = false;
+		}
+		if (Input.GetButtonDown("Horizontal") && grounded == true) {
+			Debug.Log ("Andando");
+		}
+
+
+		if(Input.GetButtonDown ("Slide") && grounded == true){
+			//Debug.Log ("Slide");
+			colisor.position = new Vector3 (colisor.position.x, colisor.position.y - 0.3f, colisor.position.z);
+			slide = true;
+			tempoTempo = 0;
+		}
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.2f, whatIsChao);
+
+		if (slide == true) {
+			tempoTempo += Time.deltaTime;
+			if (tempoTempo >= timeSlide) {
+				colisor.position = new Vector3 (colisor.position.x, colisor.position.y + 0.3f, colisor.position.z);
+				slide = false;
+			}
+		}
+
+		animacao.SetBool ("jump", !grounded);
+		animacao.SetBool ("slide", slide);
+
+
+	}
+}
---
+++ Assets/ccti/Mario/Movimento.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento : MonoBehaviour {
+
+	public float velocidadeX = 0.1f;
+	public float movimentoX;
+	public float inputX;
+	public float forcaSalto = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float inputX = Input.GetAxis ("Horizontal");
+		if (inputX > 0) {
+			movimentoX = transform.position.x + (inputX * velocidadeX);
+			transform.position = new Vector3 (movimentoX, transform.position.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if(inputX < 0) {
+			movimentoX = transform.position.x + (inputX * velocidadeX);
+			transform.position = new Vector3 (movimentoX, transform.position.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			GetComponent <Rigidbody2D>().AddForce (new Vector2(0, forcaSalto));
+		}
+
+	}
+}
